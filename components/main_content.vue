e96c274aae71d3c4d7365961ff1cb5c9955a7112
mizphses <fuminori@mizphses.com>
2022-12-15 01:15:10 +0900

    first commit

---
+++ components/main_content.vue
@@ -0,0 +1,101 @@
+<template>
+  <div>
+    <div class="header">
+      <img src="@/assets/images/logo_double.svg" alt="mizphses.com 寒中見舞いサイト" />
+    </div>
+    <div v-for="(data, index) in activities" :key="data.title">
+      <div class="nakami-top" :style="{backgroundImage: `url(${data['image']})`, backgroundSize: 'cover'}" v-if="index == randomnum">
+        <img src="@/assets/images/title.svg" alt="寒中お見舞い申し上げます" class="title_text" />
+        <div class="nakami-setumei">
+          <p class="titleline">{{data["title"]}}</p>
+          <p>{{data["body"]}}</p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const activities = [
+  {"image": "/1-1.JPG", "title": "美保の松原", "body": "富士山が綺麗でした"},
+  {"image": "/3-1.JPG", "title": "ブレードサーバー導入", "body": "長野県長野市（公有財産売却）から調達."},
+  {"image": "/3-2.JPG", "title": "ナールMほか", "body": "書体讃歌さん(文字マニアの人)から譲っていただきました．本当に感謝．"},
+  {"image": "/4-1.JPG", "title": "活版印刷", "body": "印刷博物館で活版印刷の体験を数年ぶりにしてきました．味がある．"},
+  {"image": "/5-1.JPG", "title": "BLドラマの聖地", "body": "当時住んでた家の最寄りコインランドリーがBLドラマの聖地になりました．"},
+  {"image": "/7-1.JPG", "title": "さわやか", "body": "1月以来時々食べに行きます．美味しいよね．"},
+  {"image": "/9-1.JPG", "title": "ボストン研修（Global FLP）", "body": "ボストンに大学の研修で行ってきました．写真はエジプト人男性の阿部寛．"},
+  {"image": "/9-2.JPG", "title": "引っ越し", "body": "旧居から引っ越しました．新居は防音性が高いです．"},
+  {"image": "/11-1.JPG", "title": "トド岩", "body": "小春六花のヨーグルトを食べると島流しにされます．（札幌旅行で行ってきた）"},
+  {"image": "/12-1.JPG", "title": "来年の抱負", "body": "写真のYogibo(1年半使った)のようにダラダラした1年でした．来年は色々作ります．"}
+  ]
+const randomnum = Math.floor( Math.random() * 10 );
+</script>
+
+<style lang="scss" scoped>
+.header{
+  box-sizing: border-box;
+  padding-top: 100px;
+  padding-left: 100px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  background-size: 400% 400%;/*サイズを大きくひきのばす*/
+  animation: bggradient 20s ease infinite;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  z-index: 1000;
+  img{
+    width: 100%;
+    max-width: 400px;
+  }
+}
+.nakami-top{
+  box-sizing: border-box;
+  height: 100vh;
+  width: 100vw;
+  margin: 0;
+  padding: 2em;
+  // background: #ededed;
+  .title_text{
+    position: absolute;
+    top: 10%;
+    right: 10%;
+    max-height: 80vh;
+    max-width: 60vw;
+    z-index: 200;
+  }
+  .nakami-setumei{
+    position: absolute;
+    bottom: 100px;
+    left: 100px;
+    width: 25em;
+    padding: 1em 2em 1em 2em;
+    background: white;
+    font-size: 1.3em;
+    line-height: 1;
+    z-index: 200;
+  }
+  .timeline{
+    font-weight: 700;
+    font-size: 0.7em;
+  }
+  .titleline{
+    font-weight: bold;
+    font-size: 1.3em;
+  }
+}
+  .nakami-top::before {
+    content: '';
+    background-color: rgba(0, 0, 0, 0.2);
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+
+</style>
+<script setup lang="ts">
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,152 @@
+<template>
+  <div>
+
+    <div class="sizing-message">
+      <p>本WEBサイトは1024px以上の環境でご覧ください．</p>
+    </div>
+    <div class="cover-message">
+      <div class="scrollable">
+      <h2>喪中に関するお知らせ</h2>
+      <p>平素よりお世話になっております。</p>
+      <p>本年は喪中につき、年賀状としてのコンテンツの公開を自粛させていただきます。</p>
+      <p>シンプルなご挨拶となりますが、ご理解の程よろしくお願いいたします。</p>
+      <hr />
+      <p class="ga">
+        が、何も作らないのはつまんないので<br>
+        <span class="ga ga2">寒中見舞いを公開いたします。</span>
+      </p>
+      <p class="sign">みずさわ</p>
+
+      <p style="font-size: 0.6em; opacity: 0.3;">なお，年賀状につきましては喜んでお受け取りいたします!! 送付先等についてはTwitter @mizphsesにお問い合わせください．</p>
+      </div>
+      <a href="/index2" class="close-button">寒中見舞いを見る</a>
+    </div>
+  </div>
+</template>
+<style>
+  body{
+      margin: 0;
+  }
+</style>
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&family=Open+Sans:ital,wght@0,400;0,700;1,400;1,700&family=Shippori+Mincho&family=Yuji+Boku&display=swap');
+  body, p, h1, h2, h3, h4, h5{
+    font-family: 'Noto Sans JP', sans-serif;
+  }
+
+  .sizing-message {
+    display: none;
+  }
+  @media (max-width:1024px) {
+    .sizing-message {
+      z-index: 100000;
+      color: white;
+      background-image: linear-gradient(to bottom right, rgb(18, 24, 26), rgb(27, 40, 47), rgb(13, 32, 42), rgb(0, 0, 0));
+      box-sizing: border-box;
+      height: 100vh;
+      width: 100vw;
+      padding: 2em;
+      position: absolute;
+      top: 0;
+      left: 0;
+      background-size: 400% 400%;/*サイズを大きくひきのばす*/
+      animation: bggradient 20s ease infinite;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+  .cover-message {
+    z-index: 10000;
+    color: white;
+    background-image: linear-gradient(to bottom right, rgb(18, 24, 26), rgb(27, 40, 47), rgb(13, 32, 42), rgb(0, 0, 0));
+    box-sizing: border-box;
+    height: 100vh;
+    width: 100vw;
+    padding: 2em;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-size: 400% 400%;/*サイズを大きくひきのばす*/
+    animation: bggradient 20s ease infinite;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    h2 {
+      font-size: 1.5rem;
+      margin: 0;
+      margin-bottom: 0.5rem;
+    }
+    hr{
+      margin-top: 3rem;
+      margin-bottom: 3rem;
+      width: 30%;
+      border: 1px solid #e5e5e5;
+    }
+    .sign{
+      font-family: 'yuji boku', cursive;
+      opacity: 0;
+      animation: 7s ga 5s ease forwards;
+    }
+    .scrollable{
+      overflow-y: scroll;
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+    }
+    .close-button{
+      margin-top: auto;
+      margin-left: auto;
+      position: absolute;
+      bottom: 2rem;
+      right: 2rem;
+      letter-spacing: 0.2rem;
+      background-image: linear-gradient(to bottom right, rgb(25, 50, 64), rgb(18, 106, 99), rgb(25, 150, 112));
+      color: white;
+      font-weight: 700;
+      border: 1px solid #e5e5e5;
+      border-radius: 0.5rem;
+      padding: 0.5rem 1rem;
+      font-size: 1rem;
+      cursor: pointer;
+    }
+    .ga{
+      font-family: 'Shippori Mincho', serif;
+      font-weight: 700;
+      font-size: 1.3em;
+      animation: ga 5s ease forwards;
+      opacity: 0;
+    }
+    .ga2{
+      animation-delay: 2s !important;
+    }
+    .close-button:hover{
+      background-image: linear-gradient(to top left, rgb(21, 128, 119), rgb(90, 229, 176));
+    }
+    @keyframes bggradient{
+      0% {
+        background-position: 0% 100%;
+      }
+      25% {
+        background-position: 25% 50%;
+      }
+      50% {
+        background-position: 50% 0%;
+      }
+      75% {
+        background-position: 25% 50%;
+      }
+      100% {
+        background-position: 0% 100%;
+      }
+    }
+    @keyframes ga{
+      0% {
+        opacity: 0;
+      }
+      100% {
+        opacity: 1;
+      }
+    }
+  }
+</style>
